2111f00165f18785960668bdd00395b944f4f326
Patipon Suksangeam <last3oy@last3oys-MacBook-Pro.local>
2017-03-03 19:18:30 +0700

    modifyyyyyy

--- index.php
+++ index.php
@@ -1,16 +1,47 @@
-<?php 
-$access_token = 'cuzL/CPJOOd4+CUzCYvayss3Rd7MJveN+uMb8va3XSfVYrih8pJLOMWj68xv4cXNkbO2E/Hyiq6Y3IcX7SwpXHKF3X4G5tbIv53pXFp8IKR8D3P3a+2ilPwTo1hHdpQN6Hh04HqKNGqugliqBQB2FQdB04t89/1O/w1cDnyilFU='
+<?php
+$access_token = 'cuzL/CPJOOd4+CUzCYvayss3Rd7MJveN+uMb8va3XSfVYrih8pJLOMWj68xv4cXNkbO2E/Hyiq6Y3IcX7SwpXHKF3X4G5tbIv53pXFp8IKR8D3P3a+2ilPwTo1hHdpQN6Hh04HqKNGqugliqBQB2FQdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-$headers = array('Authorization: Bearer ' . $access_token);
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-echo $result;
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
