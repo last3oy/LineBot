44884a049a93321b08d356f0bee96086018202e3
Patipon Suksangeam <last3oy@last3oys-MacBook-Pro.local>
2017-03-03 19:13:10 +0700

    modify index 2

--- index.php
+++ index.php
@@ -3,15 +3,14 @@ $access_token = 'cuzL/CPJOOd4+CUzCYvayss3Rd7MJveN+uMb8va3XSfVYrih8pJLOMWj68xv4cX
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$header = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch,CURLOPT_HTTPHEADER,$header);
-curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
-
 
