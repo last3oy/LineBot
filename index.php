543fb2932c73c7935a85870121b5f0fce99e43f1
Patipon Suksangeam <last3oy@last3oys-MacBook-Pro.local>
2017-03-03 19:10:10 +0700

    modify index

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php
-echo "hello world eiei";
+<?php 
+$access_token = 'cuzL/CPJOOd4+CUzCYvayss3Rd7MJveN+uMb8va3XSfVYrih8pJLOMWj68xv4cXNkbO2E/Hyiq6Y3IcX7SwpXHKF3X4G5tbIv53pXFp8IKR8D3P3a+2ilPwTo1hHdpQN6Hh04HqKNGqugliqBQB2FQdB04t89/1O/w1cDnyilFU='
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$header = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch,CURLOPT_HTTPHEADER,$header);
+curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
+
